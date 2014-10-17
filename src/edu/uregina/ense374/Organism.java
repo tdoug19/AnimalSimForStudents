f519ad958a5136b82bd06ad951e65873edf57f28
tdouglas <tdoug19@gmail.com>
2014-10-17 14:42:56 -0600

    Added some header info.

--- src/edu/uregina/ense374/Organism.java
+++ src/edu/uregina/ense374/Organism.java
@@ -1,5 +1,8 @@
 /**
+ * Name: Organism
  * 
+ * Description:  This class indicates an Ogranism.  An organism will 
+ * 				perhaps live at a location in our world grid. 
  */
 package edu.uregina.ense374;
 
